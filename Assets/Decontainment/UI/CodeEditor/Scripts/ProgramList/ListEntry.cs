d65345acd79d0877344f9e1c70820eb6c1b1b620
Brandon Shockley <bshoc@bellsouth.net>
2020-02-08 12:14:17 -0500

    Added program list

---
+++ Assets/Decontainment/UI/CodeEditor/Scripts/ProgramList/ListEntry.cs
@@ -0,0 +1,56 @@
+﻿using Asm;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+// TODO: Add a .Program subnamspace to Editor
+namespace Editor
+{
+    public class ListEntry : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField]
+        private Color selectedColor = Color.white;
+
+        private Program program;
+        private CodeList codeList;
+        private Color unselectedColor;
+
+        private Image image;
+        private TextMeshProUGUI tm;
+
+        void Awake()
+        {
+            image = GetComponent<Image>();
+            tm = GetComponentInChildren<TextMeshProUGUI>();
+
+            unselectedColor = image.color;
+        }
+
+        public void Init(Program program, CodeList codeList)
+        {
+            this.program = program;
+            this.codeList = codeList;
+            tm.text = program.name;
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            if (codeList.Program != program) {
+                image.color = selectedColor;
+                codeList.Program = program;
+                codeList.OnProgramChange += HandleProgramChange;
+            }
+        }
+
+        private void HandleProgramChange(Program oldProgram)
+        {
+            if (codeList.Program != program) {
+                image.color = unselectedColor;
+                codeList.OnProgramChange -= HandleProgramChange;
+            }
+        }
+    }
+}
---
+++ Assets/Decontainment/UI/CodeEditor/Scripts/ProgramList/ProgramList.cs
@@ -0,0 +1,73 @@
+﻿using Asm;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEditor;
+
+namespace Editor
+{
+    public class ProgramList : MonoBehaviour
+    {
+        [SerializeField]
+        private CodeList codeList = null;
+
+        /// Used to locate in-project program directory
+        [SerializeField]
+        private TextAsset assetLocationProgram = null;
+        [SerializeField]
+        private GameObject listEntryPrefab = null;
+
+        private string programDirectory;
+        private List<Program> programs = new List<Program>();
+
+        void Awake()
+        {
+            #if UNITY_EDITOR
+            programDirectory = Directory.GetCurrentDirectory() + "/" + Path.GetDirectoryName(AssetDatabase.GetAssetPath(assetLocationProgram));
+            #else
+            programDirectory = Directory.GetCurrentDirectory();
+            #endif
+
+            // Assemble all program files
+            string[] filePaths = Directory.GetFiles(programDirectory, "*.txt");
+            foreach (string filePath in filePaths) {
+                string programText = File.ReadAllText(filePath);
+                Program program = Assembler.Assemble(programText);
+                if (program != null) {
+                    program.name = Path.GetFileNameWithoutExtension(filePath);
+                    Debug.Log("Assembly of program " + program.name + " succeeded");
+
+                    // Setup autosave
+                    program.OnChange += () => SaveProgram(program);
+
+                    programs.Add(program);
+                } else {
+                    Debug.LogWarning("Assembly of file " + filePath + " failed");
+                }
+            }
+
+            // Populate list
+            foreach (Program program in programs) {
+                GameObject listEntry = Instantiate(listEntryPrefab, transform);
+                listEntry.GetComponent<ListEntry>().Init(program, codeList);
+            }
+        }
+
+        private void Clear()
+        {
+            for (int i = transform.childCount - 1; i >= 0; --i) {
+                Destroy(transform.GetChild(i).gameObject);
+            }
+        }
+
+        private void SaveProgram(Program program)
+        {
+            string programText = Disassembler.Disassemble(program);
+            string programPath = programDirectory + "/" + program.name + ".txt";
+            StreamWriter programFile = File.CreateText(programPath);
+            programFile.Write(programText);
+            programFile.Close();
+        }
+    }
+}
