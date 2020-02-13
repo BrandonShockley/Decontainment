2ea90fda992c28bd28a10e1f891d69e94293b62a
Thomas <dancerfool@gmail.com>
2020-02-13 17:37:21 -0500

    Abstracting beam away from laser for use in other weapons, first pass

---
+++ Assets/Decontainment/Bot/Weapons/Projectiles/Scripts/Beam.cs
@@ -0,0 +1,109 @@
+﻿using Bot;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Beam : Projectile 
+{
+    [SerializeField]
+    private float maxDistance = 100;
+    [SerializeField]
+    private float beamGrowTime = 1;
+    /// Duration after it has finished growing
+    [SerializeField]
+    private float beamDuration = 1;
+    [SerializeField]
+    private float beamFadeTime = 0.5f;
+
+    private LineRenderer lr;
+
+    protected override void SubAwake() 
+    {
+        lr = GetComponent<LineRenderer>();
+    }
+
+    protected override void Init() {
+        StartCoroutine(beamRoutine());
+    }
+
+    void Update() 
+    {
+        LayerMask mask = LayerMask.GetMask("Obstacle", "Bot");
+        RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, transform.right, maxDistance, mask);
+        int hitIndex = -1;
+        for (int i = 0; i < hits.Length; ++i) {
+            if (hits[i].collider.gameObject != shooter.gameObject) {
+                hitIndex = i;
+                break;
+            }
+        }
+
+        if (hitIndex == -1) {
+            lr.SetPosition(1, Vector3.right * maxDistance);
+        } else {
+            lr.SetPosition(1, Vector3.right * hits[hitIndex].distance);
+        }
+    }
+
+    private IEnumerator beamRoutine() {
+        // beam growth
+        {
+            float startTime = Time.time;
+            bool doOnce = false;
+            do {
+                if (doOnce)
+                    yield return null;
+
+                float alpha = Mathf.Lerp(0, 1, (Time.time - startTime) / beamFadeTime);
+                lr.endColor = Util.ModifyAlpha(lr.endColor, alpha);
+                lr.startColor = Util.ModifyAlpha(lr.endColor, alpha);
+
+                doOnce = true;
+            } while (Time.time - startTime < beamGrowTime);
+        }
+
+        // beam life
+        {
+            float startTime = Time.time;
+            bool doOnce = false;
+            do {
+                if (doOnce)
+                    break;
+
+                LayerMask mask = LayerMask.GetMask("Obstacle", "Bot");
+                RaycastHit2D[] hits = Physics2D.RaycastAll(transform.position, transform.right, maxDistance, mask);
+                int hitIndex = -1;
+                for (int i = 0; i < hits.Length; ++i) {
+                    if (hits[i].collider.gameObject != shooter.gameObject) {
+                        hitIndex = i;
+                        break;
+                    }
+                }
+                if (hitIndex != -1 && hits[hitIndex].collider.TryGetComponent<Health>(out Health bh)) {
+                    HealthEffect(bh);
+                }
+
+                doOnce = true;
+            } while (Time.time - startTime < beamDuration);
+        }
+
+        // beam death
+        {
+            float startTime = Time.time;
+            bool doOnce = false;
+            do {
+                if (doOnce)
+                    yield return null;
+
+                float alpha = Mathf.Lerp(1, 0, (Time.time - startTime) / beamFadeTime);
+                lr.endColor = Util.ModifyAlpha(lr.endColor, alpha);
+                lr.startColor = Util.ModifyAlpha(lr.endColor, alpha);
+
+                doOnce = true;
+            } while (Time.time - startTime < beamFadeTime);
+        }
+        Pools.Instance.Free(gameObject);
+    }
+
+    protected virtual void HealthEffect(Health bh) {}
+}
---
+++ Assets/Decontainment/Bot/Weapons/Projectiles/Scripts/HealingBeam.cs
@@ -0,0 +1,12 @@
+﻿using Bot;
+using UnityEngine;
+
+public class HealingBeam : Beam 
+{
+    [SerializeField]
+    private int healthRegain = 1;
+
+    protected override void HealthEffect(Health bh) {
+        bh.HealUp(healthRegain);
+    }
+}
