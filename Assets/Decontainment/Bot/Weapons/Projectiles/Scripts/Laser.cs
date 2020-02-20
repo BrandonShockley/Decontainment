f9792df46a14e0682305ed80e846980098c26fc8
Thomas <dancerfool@gmail.com>
2020-02-20 15:12:33 -0500

    Update Laser.cs

--- Assets/Decontainment/Bot/Weapons/Projectiles/Scripts/Laser.cs
+++ Assets/Decontainment/Bot/Weapons/Projectiles/Scripts/Laser.cs
@@ -3,11 +3,7 @@ using UnityEngine;
 
 public class Laser : Beam
 {
-    private static LayerMask MASK;
-    private static Trigger doOnce;
 
-    [SerializeField]
-    private int damage = 1;
     [SerializeField]
     private int damage = 1;
 
