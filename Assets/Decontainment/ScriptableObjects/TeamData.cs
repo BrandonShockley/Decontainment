6fd3c120452352f1835ec7c1b4ef4d2c3b368656
Albert Yiakiam Ko <Nimbro@Alberts-MacBook-Pro.local>
2020-02-29 04:16:35 -0500

    Added Test Changes to Prompt Branch

--- Assets/Decontainment/ScriptableObjects/TeamData.cs
+++ Assets/Decontainment/ScriptableObjects/TeamData.cs
@@ -149,10 +149,8 @@ public class TeamData : ScriptableObject
         AssetDatabase.RenameAsset(fromPath, newName);
         #else
         File.Move(fromPath, toPath);
-
         #endif
         name = newName;
-
     }
 
     public override string ToString() { return name; }
