57d47e59597394823ec51082c7838106e930ecb2
Brandon Shockley <bshoc@bellsouth.net>
2020-03-13 12:01:02 -0400

    Added match timer and victory banner

---
+++ Assets/Decontainment/UI/Match/Scripts/VictoryBanner.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Match
+{
+    public class VictoryBanner : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimationCurve curve = null;
+
+        private TextMeshProUGUI tm;
+
+        void Awake()
+        {
+            tm = GetComponentInChildren<TextMeshProUGUI>();
+
+            MatchManager.Instance.OnVictory += HandleVictory;
+            MatchManager.Instance.OnTimeUp += HandleTimeUp;
+        }
+
+        private void HandleVictory(int teamID)
+        {
+            tm.text = "Team " + teamID + " wins!";
+            StartCoroutine(GrowRoutine());
+        }
+
+        private void HandleTimeUp()
+        {
+            tm.text = "Time's up!";
+            StartCoroutine(GrowRoutine());
+        }
+
+        private IEnumerator GrowRoutine()
+        {
+            yield return new WaitForSecondsRealtime(2);
+            float timer = 0;
+            while (timer < curve.keys[curve.length - 1].time) {
+                timer += Time.unscaledDeltaTime;
+
+                Vector3 scale = transform.localScale;
+                scale.y = curve.Evaluate(timer);
+                transform.localScale = scale;
+                yield return null;
+            }
+        }
+    }
+}
